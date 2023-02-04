c9277956beb54da883bcd9bdaf7bef9a74ec8c3f
Brahim <boutique8.br@gmail.com>
2023-02-04 22:55:33 +0100

    updating menu

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ let ul = document.querySelector(".nav-menu");
 bars.addEventListener("click", function () {
     ul.classList.toggle("active-rotate")
 });
+
+document.addEventListener('click', (event) => {
+  if (ul.classList.contains('active-rotate') && !bars.contains(event.target)) {
+    ul.classList.remove('active-rotate');
+  }
+});
